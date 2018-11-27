6bfe025622275087c6b66c48b81d9a883705cf0e
aqeelat <abdullah.t.aqeel@gmail.com>
2018-11-27 18:07:15 -0500

    fixed code

--- StringOperator.java
+++ StringOperator.java
@@ -1,37 +1,59 @@
-public enum StringOperator{
+/**
+ * @author Aster Bodden Pineda
+ * @author Abdullah Alaqeel
+ * This class implements a "C type" enum using Java Enums
+ * This class defines a String Toy operator
+ *
+ */
+public enum StringOperator {
 	// all of the enum values have to implement the apply method 
-	UPPERCASE{public String apply(String str){return str.toUpperCase();}}, 
-	LOWERCASE{public String apply(String str){return str.toLowerCase();}},
-	FIRSTUPPER{public String apply(String str){
-		String[] words = str.split(" ");					
-		StringBuilder result = new StringBuilder(); // string builder to store result 
-		for(String word : words){
-			StringBuilder modWord = new StringBuilder(word);
-			modWord.setCharAt(0,Character.toUpperCase(word.charAt(0))); // convert the first char to UpperCase
-			result.append(" "+modWord); // add modified word to string builder 
+	UPPERCASE {public String apply(String str){return str.toUpperCase();}},
+	LOWERCASE {public String apply(String str){return str.toLowerCase();}},
+	FIRSTUPPER {
+		public String apply(String str) {
+			String[] words = str.split(" ");
+			StringBuilder result = new StringBuilder(); // string builder to store result
+			// adding the words
+			for (String word : words) {
+				int l = result.length();
+				result.append(" ").append(word).setCharAt(l+1, Character.toUpperCase(word.charAt(0)));
+			}
+			return result.deleteCharAt(0).toString();
 		}
-		return result.toString();
-	}};
+	},
+	CAMELCASE {
+		public String apply (String str) {
 
-	// enum fields: 
+			String[] w = str.split(" ");
+			StringBuilder result = new StringBuilder(); // string builder to store result
+			// adding the first word
+			result.append(w[0]).setCharAt(0, Character.toLowerCase(w[0].charAt(0)));
+			// adding the rest of the words
+			for (int i = 1; i < w.length; i++) {
+				int l = result.length();
+				result.append(w[i]).setCharAt(l, Character.toUpperCase(w[i].charAt(0)));
+			}
+			return result.toString();
+		}
+	};
 
 	/**
-	* @param str String to apply Operation 
-	* @return String processed afrer operation 
-	*/
+	 * @param str String to apply Operation
+	 * @return String processed afrer operation
+	 */
 	public abstract String apply(String str );
 
-	// constructor 
-	StringOperator(){
+	/**
+	 * The constructor is private because it is not needed.
+	 */
+	private StringOperator(){
 
 	}
 
 	public static void main(String[] args ){
 		String str = "Enums make things awesome";
 		System.out.println(str);
-		for(StringOperator op : StringOperator.values()){ // apply all variations 
-			System.out.println(op + " applied:"); 
-			System.out.println(op.apply(str)); 
-		}
+		for(StringOperator op : StringOperator.values()) // apply all variations
+			System.out.printf("%s applied:\n\t%s\n", op, op.apply(str));
 	}
 }
--- cStringOperator.java
+++ cStringOperator.java
@@ -1,55 +1,67 @@
 /**
-* @author Aster Bodden Pineda 
-* @author Abdullah Alaqeel
-* This class implements a "C type" enum using the "int enum Pattern"
-* This class defines a String Toy operator 
-**/
-
-/*
-* Warning This should be the way to implement, USE ENUMS instead 
-*/
+ * @author Aster Bodden Pineda
+ * @author Abdullah Alaqeel
+ * This class implements a "C type" enum using the "int enum Pattern"
+ * This class defines a String Toy operator
+ *
+ * Warning This should not be the way to implement, USE ENUMS instead
+ */
 
 public class cStringOperator{
 
-	private static final int UPPERCASE = 0; 
-	private static final int LOWERCASE = 1; 
-	private static final int FIRSTUPPER = 2; 
-
+	private static final int UPPERCASE = 0;
+	private static final int LOWERCASE = 1;
+	private static final int FIRSTUPPER = 2;
+	private static final int CAMELCASE = 3;
 	/**
-	* @param Operation the operation to be applied to str String 
-	* @param str  String to do Operation on 
-	* @return A string after the operation is done, throws IllegalArgumentException if invalid operation 
-	**/
-	public static String apply(int Operation, String str ){
+	 * @param Operation the operation to be applied to str String
+	 * @param str  String to do Operation on
+	 * @return A string after the operation is done, throws IllegalArgumentException if invalid operation
+	 **/
+	public static String apply(int Operation, String str){
 		switch (Operation){
 			case UPPERCASE: return str.toUpperCase();
 
 			case LOWERCASE: return str.toLowerCase();
 
-			case FIRSTUPPER: 
-							String[] words = str.split(" ");
-							StringBuilder result = new StringBuilder(); // string builder to store result 
-							for(String word : words){
-								StringBuilder modWord = new StringBuilder(word);
-								modWord.setCharAt(0,Character.toUpperCase(word.charAt(0))); // convert the first char to UpperCase
-								result.append(" "+modWord); // add modified word to string builder 
-							}
-							return result.toString();
+			case FIRSTUPPER:
+				String[] words = str.split(" ");
+				StringBuilder result = new StringBuilder(); // string builder to store result
+				// adding the words
+				for (String word : words) {
+					int l = result.length();
+					result.append(" ").append(word).setCharAt(l+1, Character.toUpperCase(word.charAt(0)));
+				}
+				return result.deleteCharAt(0).toString();
+			case CAMELCASE:
+				String[] w = str.split(" ");
+				StringBuilder r = new StringBuilder(); // string builder to store result
 
-			default: 
+				//adding the first word
+				r.append(w[0]).setCharAt(0, Character.toLowerCase(w[0].charAt(0)));
+				// adding the rest of the words
+				for (int i = 1; i < w.length; i++) {
+					int l = r.length();
+					r.append(w[i]).setCharAt(l, Character.toUpperCase(w[i].charAt(0)));
+				}
+
+				return r.toString();
+			default:
 				throw new IllegalArgumentException("Invalid Operation");
 		}
 
 	}
 
-// Helper function to test logical functionality 
+	// Helper function to test logical functionality
 	public static void main ( String[] args){
-		String str = "this Is the best Place to Be ";
-		String result = cStringOperator.apply(cStringOperator.UPPERCASE, str);
+		String str = "This Is the best Place to Be ";
+		String result = cStringOperator.apply(0, str);
 		System.out.println("The converted String is = " + result );
-		result = cStringOperator.apply(cStringOperator.LOWERCASE, str);
+		result = cStringOperator.apply(1, str);
 		System.out.println("The converted String is = " + result );
-		result = cStringOperator.apply(cStringOperator.FIRSTUPPER, str);
+		result = cStringOperator.apply(2, str);
+		System.out.println("The converted String is = " + result );
+		result = cStringOperator.apply(3, str);
 		System.out.println("The converted String is = " + result );
 	}
 
